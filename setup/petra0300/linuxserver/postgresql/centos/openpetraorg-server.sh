efabf8abb7bc39d2f7ff99a8d2774e31b348425a
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2013-07-31 19:37:07 +0200

    fix for openpetra-server init, to work on machine with IPv6 enabled. avoid error: psql: FATAL:  Ident authentication failed for user "petraserver"

--- setup/petra0300/linuxserver/postgresql/centos/openpetraorg-server.sh
+++ setup/petra0300/linuxserver/postgresql/centos/openpetraorg-server.sh
@@ -137,6 +137,7 @@ init() {
     service postgresql-$POSTGRESQLVERSION start
 
     echo "local  $OPENPETRA_DBNAME $OPENPETRA_DBUSER   md5" > /var/lib/pgsql/$POSTGRESQLVERSION/data/pg_hba.conf.new
+    echo "host  $OPENPETRA_DBNAME $OPENPETRA_DBUSER  ::1/128   md5" >> /var/lib/pgsql/$POSTGRESQLVERSION/data/pg_hba.conf.new
     echo "host  $OPENPETRA_DBNAME $OPENPETRA_DBUSER  127.0.0.1/32   md5" >> /var/lib/pgsql/$POSTGRESQLVERSION/data/pg_hba.conf.new
     cat /var/lib/pgsql/$POSTGRESQLVERSION/data/pg_hba.conf >> /var/lib/pgsql/$POSTGRESQLVERSION/data/pg_hba.conf.new
     mv -f /var/lib/pgsql/$POSTGRESQLVERSION/data/pg_hba.conf.new /var/lib/pgsql/$POSTGRESQLVERSION/data/pg_hba.conf
